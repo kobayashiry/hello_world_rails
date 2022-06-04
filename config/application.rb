95ecea084cfde414b5c9c715a2dd06f0c203fd4c
小林亮介 <kobaryo.gwin@gmail.com>
2022-06-04 17:00:57 +0900

    Articles APIの雛形をscaffoldコマンドにより作成

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,10 @@ module HelloWorldRails
       g.stylesheets false
       g.helper false
       g.test_framework false
+      g.template_engine false
     end
+
+    config.api_only = true
+
   end
 end
