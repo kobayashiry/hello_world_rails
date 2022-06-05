746986532ff3f2856ebd0ed9beeb434cd31d8581
小林亮介 <kobaryo.gwin@gmail.com>
2022-06-05 19:55:34 +0900

    CRUD APIの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,43 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
   def index
     @users = User.all
     # render json: @users
   end
 
   def show
-    @user = User.find(params[:id])
   end
+
+  def create
+    #インスタンスをmodelから作成する
+    @user = User.new(user_params)
+    #インスタンスをDBに保存する
+    @user.save!
+    #jsonとして値を返す
+    render :show
+  end
+
+  def update
+    # 探してきたレコードに対して変更を行う
+    @user.update!(user_params)
+    # jsonとして値を返す
+    render :show
+  end
+
+  def destroy
+    # 探してきたレコードを削除する
+    @user.destroy!
+    #jsonとして値を返す
+    render :show
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :account, :email)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
